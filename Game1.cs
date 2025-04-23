0c110d7ff108e195c7a77640eb242248a5934702
Riina Paaso <s4pari01@students.osao.fi>
2025-04-23 15:25:33 +0300

    Added multiple types of fish and a new scoring system

--- Game1.cs
+++ Game1.cs
@@ -1,10 +1,18 @@
 ﻿using System;
+using System.Collections.Generic;
 using Microsoft.Xna.Framework;
 using Microsoft.Xna.Framework.Graphics;
 using Microsoft.Xna.Framework.Input;
 
 namespace FishingGame
 {
+    public struct Fish
+    {
+        public string Name;
+        public int Points;
+        public Texture2D Texture;
+    }
+
     public class Game1 : Game
     {
 
@@ -13,14 +21,20 @@ namespace FishingGame
         private SpriteBatch _spriteBatch;
         private GraphicsDevice _device;
         private SpriteFont _font;
-        private int _fishCounter = 0;
+
+        private int _fishPoints = 0;
         private bool _fishBites = false;
         private bool _fishingRodCast = false;
+        private bool _fishEscaped = false;
         private Random _randomiser = new Random();
         private float _biteTimer;
         private float _biteInterval;
         private float _catchTimer;
         private float _catchTimerLimit = 2;
+
+        private Fish? _caughtFish;
+        private Fish? _nextFish;
+        private List<Fish> _fishTypes = new List<Fish>();
 
         private KeyboardState _currentKbState;
         private KeyboardState _previousKbState;
@@ -51,7 +65,18 @@ namespace FishingGame
 
             // TODO: use this.Content to load your game content here
             _font = Content.Load<SpriteFont>("myFont");
-            _biteInterval = (float)_randomiser.Next(2, 6);
+
+            _fishTypes = new List<Fish>
+            {
+                new Fish {Name = "Salmon", Points = 10},
+                new Fish {Name = "Pike", Points = 8},
+                new Fish {Name = "Trout", Points = 5},
+                new Fish {Name = "Perch", Points = 3},
+                new Fish {Name = "Bream", Points = 1},
+                new Fish {Name = "Tin Can", Points = 0}
+            };
+
+            ResetFish();
         }
 
         private bool IsKeyPressed (Keys key)
@@ -65,6 +90,7 @@ namespace FishingGame
             _fishingRodCast = false;
             _biteTimer = 0;
             _biteInterval = _randomiser.Next(2, 6);
+            _nextFish = _fishTypes[_randomiser.Next(_fishTypes.Count)];
         }
 
         private void FishBiteTimer(GameTime gameTime)
@@ -75,6 +101,8 @@ namespace FishingGame
             if (!_fishingRodCast && IsKeyPressed(Keys.Space))
             {
                 _fishingRodCast = true;
+                _fishEscaped = false;
+                _caughtFish = null;
             }
 
             if (_fishingRodCast)
@@ -95,13 +123,15 @@ namespace FishingGame
 
                     if (IsKeyPressed(Keys.Space))
                     {
-                        _fishCounter++;
+                        _caughtFish = _nextFish;
+                        _fishPoints += _caughtFish.Value.Points;
                         ResetFish();
                     }
 
                     if (_catchTimer <= 0)
                     {
                         ResetFish();
+                        _fishEscaped = true;
                     }
                 }
             }
@@ -133,8 +163,14 @@ namespace FishingGame
 
         private void DrawText()
         {
-            _spriteBatch.DrawString(_font, "Fish caught: " + _fishCounter.ToString(), new Vector2(20,45), Color.White);
-            _spriteBatch.DrawString(_font, "Time until fish bites: " + _biteInterval.ToString(), new Vector2(20, 280), Color.White);
+            _spriteBatch.DrawString(_font, $"Points: {_fishPoints}", new Vector2(20,45), Color.White);
+            _spriteBatch.DrawString(_font, $"Time until fish bites: {_biteInterval}", new Vector2(20, 450), Color.White);
+
+            if (_fishEscaped)
+            {
+                _spriteBatch.DrawString(_font, "The fish got away!", new Vector2(20, 85), Color.White);
+            }
+
             if (!_fishingRodCast)
             {
                 _spriteBatch.DrawString(_font, "Press Space to cast your fishing rod.", new Vector2(20, 105), Color.White);
@@ -142,6 +178,11 @@ namespace FishingGame
             else if (_fishBites)
             {
                 _spriteBatch.DrawString(_font, "Fish hooked! Press Space to catch it.", new Vector2(20, 105), Color.White);
+            }
+
+            if (_caughtFish.HasValue)
+            {
+                _spriteBatch.DrawString(_font, $"You caught a {_caughtFish.Value.Name}!", new Vector2(20, 125), Color.White);
             }
         }
     }
